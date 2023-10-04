bc7ad4a526981e91815ed8e71a2575212b5e00c6
Julien Cros <julien.cros86@gmail.com>
2023-10-04 22:57:08 +0200

    fix getStartedbutton

--- src/components/GetStartedButton.tsx
+++ src/components/GetStartedButton.tsx
@@ -7,7 +7,7 @@ const GetStartedButton = () => {
     <div>
       <button
         className="bg-amber-800 text-pale-red px-4 py-2 rounded-full hover:bg-pale-red hover:text-amber-800 hover:scale-105 transition duration-105 ease-out active:scale-95 shadow-lg active:shadow-xl"
-        onClick={() => signIn()}
+        onClick={() => signIn("google")}
       >
         Get Started
       </button>
