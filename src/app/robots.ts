c7600af7a284f0e734de9dc5995d85b43c579371
Julien Cros <julien.cros86@gmail.com>
2024-02-19 19:17:53 +0100

    feat: add robots

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',	
+      disallow: '/create-card',
+    },
+    sitemap: 'https://koffy.app/sitemap.xml',
+  }
+}
