eddc759f26682a760233dfa324b8f50c51a878bb
Julien Cros <julien.cros86@gmail.com>
2024-02-19 18:43:37 +0100

    feat: add metadata for each post page

--- src/app/coffee-list/[id]/page.tsx
+++ src/app/coffee-list/[id]/page.tsx
@@ -1,21 +1,58 @@
 import ExpandedCard from "@/components/ExpandedCard";
 import { getPostFromId, getUserFromId } from "@/lib/actions";
+import { Metadata } from "next";
 
+
+// TODO: change id to title, brand, author, or something concret and unique
 type PageProps = {
   params: {
     id: string;
   };
 };
 
-const page = async ({ params }: PageProps) => {
-	const user = await getUserFromId(params.id);
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata | undefined> {
+  if (!params?.id) {
+    return;
+  }
+  
 	const post = await getPostFromId(params.id);
 
-	const isMine = user ? true : false;
+  if (!post) {
+    return;
+  }
+
+  return {
+    metadataBase: new URL("https://espressive.fr"),
+    title: post.title,
+    ...{ keywords: (post.title, post.brand, post.variety) },
+		description: (post.tasting || ''),
+    openGraph: {
+      title: post.title,
+			description: (post.tasting || ''),
+      url: `https://espressive.fr/post/${post.id}`,
+      type: "article",
+      publishedTime: post.createdAt.toISOString(),
+      modifiedTime: post.updatedAt.toISOString(),
+    },
+    twitter: {
+      title: post.title,
+      description: post.tasting || '',
+      card: "summary_large_image",
+    },
+  };
+}
+
+const page = async ({ params }: PageProps) => {
+  const user = await getUserFromId(params.id);
+  const post = await getPostFromId(params.id);
+
+  const isMine = user ? true : false;
   return (
-	<div>
-		<ExpandedCard post={post} id={params.id} isMine={isMine}/>
-	</div>
+    <div>
+      <ExpandedCard post={post} id={params.id} isMine={isMine} />
+    </div>
   );
 };
 
