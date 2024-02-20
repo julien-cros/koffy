556add7438400edde02d4be3c64f8f7ae90bbf26
Julien Cros <julien.cros86@gmail.com>
2024-02-20 16:35:06 +0100

    test: rm map of posts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,31 +1,9 @@
 import { MetadataRoute } from "next";
-import { findSearchPost } from "@/app/search-page/actions";
 
 const BASE_URL = 'https://koffy.app'
 
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-
-	const allPost = await findSearchPost('brand', '', 'public');
-
-	if (!allPost) return [
-		{
-			url: `${URL}/coffee-list`,
-			lastModified: new Date(),
-			priority: 0.5,
-		},
-		{
-			url: `${URL}/search-page/brand--public`,
-			lastModified: new Date(),
-			priority: 1,
-		}
-	];
-
-		const posts = allPost.map((post) => ({
-			url: `${BASE_URL}/${post.id}`,
-			lastModified: new Date(post.updatedAt),
-			priority: 0.7,
-		}));
 	
 		return [
 			{
@@ -43,6 +21,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 				lastModified: new Date().toISOString().split("T")[0],
 				priority: 1,
 			},
-			...posts,
+
 		];
 	}
