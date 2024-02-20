19e2032ebef78665cb30f4eba6b5b7c20f0716d2
Julien Cros <julien.cros86@gmail.com>
2024-02-20 16:38:24 +0100

    test: just keep /

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,18 +9,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 			{
 				url: BASE_URL,
 				lastModified: new Date().toISOString().split("T")[0],
-				priority: 1,
+				priority: 0.7,
 			},
-			{
-				url: `${URL}/coffee-list`,
-				lastModified: new Date().toISOString().split("T")[0],
-				priority: 0.5,
-			},
-			{
-				url: `${URL}/search-page/brand--public`,
-				lastModified: new Date().toISOString().split("T")[0],
-				priority: 1,
-			},
-
 		];
 	}
