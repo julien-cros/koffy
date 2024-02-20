cc793235532cc1273bc584bb09eee72b3411119a
Julien Cros <julien.cros86@gmail.com>
2024-02-20 15:52:31 +0100

    feat: add metadata for coffee-list page

--- src/app/coffee-list/page.tsx
+++ src/app/coffee-list/page.tsx
@@ -3,7 +3,28 @@ import { getUserPosts } from "@/lib/actions";
 import { getCurrentUser } from "@/lib/session";
 import React from "react";
 import CoffeeListComponent from "@/components/CoffeeListComponent";
+import { Metadata } from "next";
 
+export async function generateMetadata(): Promise<Metadata | undefined> {
+
+  return {
+    metadataBase: new URL("https://koffy.app"),
+    title: "Your Coffee List",
+    ...{ keywords: "your coffee, coffee list, coffee history, coffee records"},
+		description: "A list of all the coffee you have ever tasted",
+    openGraph: {
+      title: "Your Coffee List",
+		description: "A list of all the coffee you have ever tasted",
+      url: `https://koffy.app/coffee-list`,
+      type: "website",
+    },
+    twitter: {
+      title: "Your Coffee List",
+      description: "A list of all the coffee you have ever tasted",
+      card: "summary_large_image",
+    },
+  };
+}
 
 export default async function CoffeeListPage() {
   const session = await getCurrentUser();
