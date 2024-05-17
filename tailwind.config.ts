a62be97fa39da8c9f8dd10156e7617b7a3a67f78
Julien Cros <julien.cros86@gmail.com>
2024-05-17 19:09:31 +0200

    style: tailwind width transition

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,74 +1,77 @@
 import type { Config } from "tailwindcss";
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 
 const svgToDataUri = require("mini-svg-data-uri");
- 
+
 const {
-  default: flattenColorPalette,
+	default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        hero: "url('/public/collection-background.svg')",
-      },
-      colors: {
-        primary: "#ECEEFF",
-        "pale-red": "#F5F0E6",
-        "coral-red": "#FF6452",
-        "slate-gray": "#6D6D6D",
-        "pale-blue": "#F5F6FF",
-        "white-400": "rgba(255, 255, 255, 0.80)",
-      },
-      boxShadow: {
-        "3xl": "0 10px 40px rgba(0, 0, 0, 0.1)",
-      },
-      //   backgroundImage: {
-      // 	'hero': "url('assets/images/collection-background.svg')",
-      // 	'card': "url('assets/images/thumbnail-background.svg')",
-      //   },
-      screens: {
-        wide: "1440px",
-      },
-    },
-  },
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+				hero: "url('/public/collection-background.svg')",
+			},
+			colors: {
+				primary: "#ECEEFF",
+				"pale-red": "#F5F0E6",
+				"coral-red": "#FF6452",
+				"slate-gray": "#6D6D6D",
+				"pale-blue": "#F5F6FF",
+				"white-400": "rgba(255, 255, 255, 0.80)",
+			},
+			boxShadow: {
+				"3xl": "0 10px 40px rgba(0, 0, 0, 0.1)",
+			},
+			//   backgroundImage: {
+			// 	'hero': "url('assets/images/collection-background.svg')",
+			// 	'card': "url('assets/images/thumbnail-background.svg')",
+			//   },
+			screens: {
+				wide: "1440px",
+			},
+			transitionProperty: {
+				'width': 'width'
+			},
+		},
+	},
 	darkMode: "class",
-  plugins: [
+	plugins: [
 		nextui(),
 		function ({ matchUtilities, theme }: any) {
-      matchUtilities(
-        {
-          "bg-grid": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-grid-small": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-dot": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-      );
-    },
+			matchUtilities(
+				{
+					"bg-grid": (value: any) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+						)}")`,
+					}),
+					"bg-grid-small": (value: any) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+						)}")`,
+					}),
+					"bg-dot": (value: any) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+						)}")`,
+					}),
+				},
+				{ values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+			);
+		},
 	],
 };
 
