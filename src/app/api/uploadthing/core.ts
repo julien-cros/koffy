e3fa061b8ef9f8430b84ce1c1e38cbc304a76941
Julien Cros <julien.cros86@gmail.com>
2024-06-21 13:13:58 +0200

    fest: biome + core

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,32 +3,33 @@ import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId", req: req });
+const auth = (req: Request) => ({ id: "fakeId", req: req }); // Fake auth function
+
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
+	// Define as many FileRoutes as you like, each with a unique routeSlug
+	imageUploader: f({ image: { maxFileSize: "4MB" } })
+		// Set permissions and file types for this FileRoute
+		.middleware(async ({ req }) => {
+			// This code runs on your server before upload
+			const user = await auth(req);
 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
+			// If you throw, the user will not be able to upload
+			if (!user) throw new UploadThingError("Unauthorized");
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
+			// Whatever is returned here is accessible in onUploadComplete as `metadata`
+			return { userId: user.id };
+		})
+		.onUploadComplete(async ({ metadata, file }) => {
+			// This code RUNS ON YOUR SERVER after upload
+			console.log("Upload complete for userId:", metadata.userId);
 
-      console.log("file url", file.url);
+			console.log("file url", file.url);
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
-    }),
+			// !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+			return { uploadedBy: metadata.userId };
+		}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
